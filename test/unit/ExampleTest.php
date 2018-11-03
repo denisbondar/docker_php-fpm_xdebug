ff12fc740010fab686d6e41a20690a80ec9a434b
Denis Bondar <bondar.den@gmail.com>
2018-11-03 18:51:18 +0200

    Add phpunit.

---
+++ test/unit/ExampleTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by Bondar Denis <bondar.den@gmail.com>
+ * Date: 03.11.18 18:33
+ */
+
+namespace TestUnit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testOk()
+    {
+        $this->assertTrue(true);
+    }
+}
