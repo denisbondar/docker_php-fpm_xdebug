d7916c814a5238ea7a4d41fa3d189f43a5ac4e60
Denis Bondar <bondar.den@gmail.com>
2019-12-04 14:38:15 +0200

    Simplify the alias.bash

--- alias.bash
+++ alias.bash
@@ -1,89 +1,27 @@
 ################################################################################
 ################################################################################
 
-## DOCKER ENVIRONMENT ALIASES
-
-env-up() {
-  docker-compose up -d
-}
-
-env-stop() {
-  docker-compose stop
-}
-
-env-restart() {
-  env-stop
-  env-up
-}
-
-env-build() {
-  docker-compose build
-}
-
-env-pull() {
-  docker-compose pull
-}
-
-env-rebuild() {
-  env-build
-  env-restart
-  permissions
-}
-
-env-logs() {
-  docker-compose logs "$@"
-}
-
-env-init() {
-  env-pull
-  env-build
-  env-restart
-  composer --prefer-dist install
-  permissions
-}
-
-env-destroy() {
-  docker-compose down -v --remove-orphans
-}
-
-################################################################################
-################################################################################
-
 # PROXIES (to Docker-containers commands)
-
-composer() {
-  docker-compose run --rm php-fpm composer "$@"
-}
-
-codecept() {
-  docker-compose run --rm php-fpm vendor/bin/codecept "$@"
-}
+alias php="docker-compose run --rm php-fpm"
+alias composer="docker-compose run --rm php-fpm composer"
+alias codecept="docker-compose run --rm php-fpm ./vendor/bin/codecept"
 
 # Symfony
-
-console() {
-  docker-compose run --rm php-fpm php bin/console "$@"
-}
+alias console="docker-compose run --rm php-fpm php ./bin/console"
 
 phpunit() {
-  EXECUTE="vendor/bin/phpunit"
-  if [ -f "bin/phpunit" ]; then
-      EXECUTE="bin/phpunit"
+  EXECUTE="./vendor/bin/phpunit"
+  if [ -f "./bin/phpunit" ]; then
+      EXECUTE="./bin/phpunit"
   fi
   docker-compose run --rm php-fpm ${EXECUTE} "$@"
 }
 
 # Laravel
-
-artisan() {
-  docker-compose run --rm php-fpm php artisan "$@"
-}
+alias artisan="docker-compose run --rm php-fpm php artisan"
 
 # Yii2
-
-yii() {
-  docker-compose run --rm php-fpm php yii "$@"
-}
+alias yii="docker-compose run --rm php-fpm php yii"
 
 ################################################################################
 ################################################################################
@@ -93,4 +31,26 @@ yii() {
 permissions() {
   sudo chown -R "${USER}":"${USER}" ./
   sudo chmod -R u=rwX,g=rwX,o=rwX ./
+}
+
+################################################################################
+################################################################################
+
+## DOCKER ENVIRONMENT ALIASES
+
+alias env-up="docker-compose up -d"
+alias env-stop="docker-compose stop"
+alias env-restart="env-stop && env-up"
+alias env-build="docker-compose build"
+alias env-pull="docker-compose pull"
+alias env-rebuild="env-build && env-restart && permissions"
+alias env-logs="docker-compose logs"
+alias env-destroy="docker-compose down -v --remove-orphans"
+
+env-init() {
+  env-pull
+  env-build
+  env-restart
+  composer --prefer-dist install
+  permissions
 }
